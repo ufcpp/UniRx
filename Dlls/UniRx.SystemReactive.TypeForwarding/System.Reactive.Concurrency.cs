fb8a72c4603c4656b259fb2b7f8e79c89eeb8863
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-06-28 15:23:10 +0900

    support netstandard1.3

--- Dlls/UniRx.SystemReactive.TypeForwarding/System.Reactive.Concurrency.cs
+++ Dlls/UniRx.SystemReactive.TypeForwarding/System.Reactive.Concurrency.cs
@@ -4,5 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: TypeForwardedTo(typeof(IScheduler))]
 [assembly: TypeForwardedTo(typeof(Scheduler))]
 [assembly: TypeForwardedTo(typeof(CurrentThreadScheduler))]
+#if NET45
 [assembly: TypeForwardedTo(typeof(ThreadPoolScheduler))]
+#endif
 [assembly: TypeForwardedTo(typeof(ImmediateScheduler))]
