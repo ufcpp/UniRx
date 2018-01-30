3018ba8c2731a133667723438813accdfc683131
ppc <ppc_kh@yahoo.co.jp>
2018-01-30 17:09:06 +0900

    1.2.0 SyncrhonizationContextを受け取るObserveOn()を追加

---
+++ Assets/Plugins/UniRx/Scripts/ObservableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+#if SystemReactive
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using System.Threading;
+using UniRx;
+
+namespace System.Reactive.Linq
+#else
+using UniRx.Operators;
+using System.Threading;
+
+namespace UniRx
+#endif
+{
+    public static partial class Observable
+    {
+        /// <summary>
+        /// observe on synchronizationContext
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="synchronizationContext"></param>
+        /// <returns></returns>
+        public static IObservable<T> ObserveOn<T>(this IObservable<T> source, SynchronizationContext synchronizationContext)
+        {
+            return Create<T>(x =>
+            {
+                return source.Subscribe(arg =>
+                {
+                    if (synchronizationContext == null || synchronizationContext == SynchronizationContext.Current)
+                    {
+                        x.OnNext(arg);
+                    }
+                    else
+                    {
+                        synchronizationContext.Post(y => x.OnNext((T)y), arg);
+                    }
+                });
+            });
+        }
+    }
+}
